d0ed32b6c873c12b7794ddf08f3eefc5f6fa92f7
Nicolas <narunzinhonn@gmail.com>
2022-07-15 23:46:08 -0300

    Att bolinhas

--- main.js
+++ main.js
@@ -1,7 +1,124 @@
-let show = document.querySelector(".texto");
-let c = document.querySelector(".circulo");
+let oris = document.querySelector(".texto");
+let o = document.querySelector(".circulo");
 
-c.addEventListener('click', () => {
-    show.style.visibility= 'visible';
-    show.style.transition = "2s";
+o.addEventListener('mouseover', () => {
+    oris.style.opacity = '1';
+   
 })
+
+o.addEventListener('mouseout', () => {
+    oris.style.opacity = '0';
+
+})
+// -----------------------------
+
+let hour = document.querySelector(".texto1");
+let  h = document.querySelector(".circulo1");
+
+h.addEventListener('mouseover', () => {
+    hour.style.opacity = '1';
+   
+})
+
+h.addEventListener('mouseout', () => {
+    hour.style.opacity = '0';
+
+})
+// -----------------------------
+let aza = document.querySelector(".texto2");
+let  a = document.querySelector(".circulo2");
+
+a.addEventListener('mouseover', () => {
+    aza.style.opacity = '1';
+   
+})
+
+a.addEventListener('mouseout', () => {
+    aza.style.opacity = '0';
+
+})
+// -----------------------------
+let cacty = document.querySelector(".texto3");
+let  ca = document.querySelector(".circulo3");
+
+ca.addEventListener('mouseover', () => {
+    cacty.style.opacity = '1';
+   
+})
+
+ca.addEventListener('mouseout', () => {
+    cacty.style.opacity = '0';
+
+})
+// -----------------------------
+let parin = document.querySelector(".texto4");
+let  p = document.querySelector(".circulo4");
+
+p.addEventListener('mouseover', () => {
+    parin.style.opacity = '1';
+   
+})
+
+p.addEventListener('mouseout', () => {
+    parin.style.opacity = '0';
+
+})
+// -----------------------------
+
+let rocka = document.querySelector(".texto5");
+let  r = document.querySelector(".circulo5");
+
+r.addEventListener('mouseover', () => {
+    rocka.style.opacity = '1';
+   
+})
+
+r.addEventListener('mouseout', () => {
+    rocka.style.opacity = '0';
+
+})
+// -----------------------------
+
+let tewnz = document.querySelector(".texto6");
+let  te = document.querySelector(".circulo6");
+
+te.addEventListener('mouseover', () => {
+    tewnz.style.opacity = '1';
+   
+})
+
+te.addEventListener('mouseout', () => {
+    tewnz.style.opacity = '0';
+
+})
+
+// -----------------------------
+
+let shurin = document.querySelector(".texto7");
+let  sh = document.querySelector(".circulo7");
+
+sh.addEventListener('mouseover', () => {
+    shurin.style.opacity = '1';
+   
+})
+
+sh.addEventListener('mouseout', () => {
+    shurin.style.opacity = '0';
+
+})
+
+// -----------------------------
+
+let viridias = document.querySelector(".texto8");
+let  vi = document.querySelector(".circulo8");
+
+vi.addEventListener('mouseover', () => {
+    viridias.style.opacity = '1';
+   
+})
+
+vi.addEventListener('mouseout', () => {
+    viridias.style.opacity = '0';
+
+})
+// -----------------------------
